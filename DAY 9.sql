d5a0d769a1154727a6e1047252b3eacd368fa5d6
Jashwanth G <codesjash@gmail.com>
2025-12-20 20:13:18 +0530

    9

--- DAY 9.sql
+++ DAY 9.sql
@@ -2,19 +2,25 @@
 -- Show first_name, last_name, and address.
 -- -- Goal: Ensure we see all customers, even if their address is missing (though in Sakila, 
 -- data is pretty clean, so you might not see NULLs, but the query structure matters).
-
-
-
+select customer.first_name, customer.last_name, address.address
+from customer
+left join address on customer.address_id = address.address_id;
 
 
 -- -- Inventory Check: Write a query to join film (left) and inventory (right) on film_id.
 -- -- Select: film.title, inventory.inventory_id.
 -- -- Goal: This will list all films. If a film is not in stock (not in inventory), 
 -- the inventory_id will be NULL.
-
+select inventory.inventory_id,film.title
+from film
+left join inventory on film.film_id = inventory.film_id;
 
 
 -- -- The "Unrented Films" Report (The IS NULL Trick):
 -- -- Using the query from Q2, add a WHERE clause to find films that exist in the database 
 -- but are NOT in the inventory.
 -- -- Hint: WHERE inventory.inventory_id IS NULL.
+select inventory.inventory_id,film.title
+from film
+left join inventory on film.film_id = inventory.film_id
+where inventory.inventory_id is null;
