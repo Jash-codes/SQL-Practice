3bb77977855e07ee897bfbedd25360a63287ebce
Jashwanth G <codesjash@gmail.com>
2025-12-20 20:47:25 +0530

    11

--- DAY 11.sql
+++ DAY 11.sql
@@ -1,7 +1,7 @@
 -- Write a query to retrieve the title of the film and the name of the category 
 -- (rename it as category_name). Filter: Only show rows where the category name is 'Horror'.
 
-select film.title, category.name
+select film.title, category.name as category_name
 from film
 inner join film_category on film.film_id  = film_category.film_id
 inner join category on film_category.category_id = category.category_id
